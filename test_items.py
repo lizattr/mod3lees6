b216e927df3e48b8eb2c887a92c78a8cce185226
Lisa <lizatrue5@gmail.com>
2023-02-10 11:01:55 +0600

    Запуск браузера и поиск кнопки

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import time
+from selenium.webdriver.common.by import By
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_exist(browser):
+    browser.get(link)
+    time.sleep(3)
+    buttons = browser.find_elements(By.XPATH, "//button[@class = 'btn btn-lg btn-primary btn-add-to-basket']")
+    print(buttons)
+    assert len(buttons) > 0, 'Кнопки не найдено'
