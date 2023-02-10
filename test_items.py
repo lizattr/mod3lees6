37b1244917fd255b3a3f842d8235ef541bb0285e
Lisa <lizatrue5@gmail.com>
2023-02-10 11:12:59 +0600

    Добавила time(sleep)

--- test_items.py
+++ test_items.py
@@ -7,7 +7,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_exist(browser):
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     buttons = browser.find_elements(By.XPATH, "//button[@class = 'btn btn-lg btn-primary btn-add-to-basket']")
     print(buttons)
     assert len(buttons) > 0, 'Кнопки не найдено'
