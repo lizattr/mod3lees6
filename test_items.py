9fbc59591cf7fab1d44420de4a80b17c796fff3e
Lisa <lizatrue5@gmail.com>
2023-02-10 11:47:52 +0600

    Небольшая правка в названии функции

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_exist(browser):
+def test_exist_button(browser):
     browser.get(link)
     time.sleep(30)
     buttons = browser.find_elements(By.XPATH, "//button[@class = 'btn btn-lg btn-primary btn-add-to-basket']")
